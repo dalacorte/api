1cb296078ea9ea0ce34e4b0d4e2650716f9a7a32
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-01 22:07:25 -0300

    extensions

---
+++ Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Api.Dto;
+using Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public static class Extensions
+    {
+        public static UserDTO AsDTO(this User user)
+        {
+            return new UserDTO
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Name = user.Name,
+                ProfilePicture = user.ProfilePicture,
+                EmailVerified = user.EmailVerified
+            };
+        }
+    }
+}
