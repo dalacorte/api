21aa8b7ab05b48e72392f16752dd60b2baf0854b
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-01 22:07:54 -0300

    user data transfer object

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dto
+{
+    public class UserDTO
+    {
+        public Guid Id { get; init; }
+        public string Email { get; init; }
+        public string Name { get; init; }
+        public string ProfilePicture { get; init; }
+        public bool EmailVerified { get; init; }
+    }
+}
