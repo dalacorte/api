2472587bd45641c057d37e431549990285f5d2b1
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-06 00:21:47 -0300

    refactoring

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Api.Repository
+namespace Api.Repositories
 {
     public class UserRepository : IUserRepository
     {
