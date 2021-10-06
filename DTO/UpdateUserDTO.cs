22a588b6f0cbacf8dcb7424344c4e2a2137bed88
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-05 21:39:34 -0300

    update user dto

---
+++ DTO/UpdateUserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dto
+{
+    public class UpdateUserDTO
+    {
+        [Required]
+        public string Email { get; init; }
+        [Required]
+        public string Name { get; init; }
+        public string ProfilePicture { get; init; }
+        [Required]
+        public bool EmailVerified { get; init; }
+    }
+}
