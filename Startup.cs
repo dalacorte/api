e0bf23fc955ac4dea67cdd8958d6fa685a5b0644
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-08 23:09:29 -0300

    refactoring

--- Startup.cs
+++ Startup.cs
@@ -60,7 +60,8 @@ namespace Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
             }
 
-            app.UseHttpsRedirection();
+            if(env.IsDevelopment())
+                app.UseHttpsRedirection();
 
             app.UseRouting();
 
