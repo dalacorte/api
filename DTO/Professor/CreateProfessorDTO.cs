c124e25294b0c88d80ceba55834e34b6427b920d
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-12 01:36:25 -0300

    refactoring

--- DTO/Professor/CreateProfessorDTO.cs
+++ DTO/Professor/CreateProfessorDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Api.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,12 +7,20 @@ using System.Threading.Tasks;
 
 namespace Api.Dto
 {
-    public record UpdateUserDTO
+    public record CreateProfessorDTO
     {
+        [Required]
         public string Email { get; init; }
+        [Required]
         public string Name { get; init; }
+        [Required]
+        public string Username { get; init; }
+        [Required]
         public string Password { get; init; }
+        [Required]
+        public IEnumerable<Post> Post { get; init; }
         public string ProfilePicture { get; init; }
+        [Required]
         public bool EmailVerified { get; init; }
     }
 }
---
+++ Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUser(Guid id);
+        Task<IEnumerable<User>> GetUser();
+        Task CreateUser(User user);
+        Task UpdateUser(User user);
+        Task DeleteUser(Guid id);
+    }
+}
