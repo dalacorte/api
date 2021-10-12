68cbf9a0596158fa072ec97eba7426d04b007f92
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-12 01:37:58 -0300

    professor dto

--- Extensions.cs
+++ Extensions.cs
@@ -9,18 +9,35 @@ namespace Api
 {
     public static class Extensions
     {
-        public static UserDTO AsDTO(this User user)
+        public static UserDTO AsUserDTO(this User user)
         {
             return new UserDTO
             {
                 Id = user.Id,
                 Email = user.Email,
                 Name = user.Name,
-                Username = user.Name,
+                Username = user.Username,
                 Password = user.Password,
                 ProfilePicture = user.ProfilePicture,
                 EmailVerified = user.EmailVerified,
                 CreatedDate = user.CreatedDate
+            };
+        }
+
+        public static ProfessorDTO AsProfessorDTO(this Professor professor)
+        {
+            return new ProfessorDTO
+            {
+                Id = professor.Id,
+                Email = professor.Email,
+                Name = professor.Name,
+                Username = professor.Username,
+                Password = professor.Password,
+                Post = professor.Post,
+                ProfilePicture = professor.ProfilePicture,
+                EmailVerified = professor.EmailVerified,
+                CanCreate = professor.CanCreate,
+                CreatedDate = professor.CreatedDate
             };
         }
     }
