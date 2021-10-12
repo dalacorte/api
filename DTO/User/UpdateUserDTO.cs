01af82f73358e51c9c5d3f1c8e55aee186936bf5
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-12 01:37:33 -0300

    refactoring

---
+++ DTO/User/UpdateUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dto
+{
+    public record UpdateUserDTO
+    {
+        public string Email { get; init; }
+        public string Name { get; init; }
+        public string Password { get; init; }
+        public string ProfilePicture { get; init; }
+        public bool EmailVerified { get; init; }
+    }
+}
