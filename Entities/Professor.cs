028e3ff1338f480a2bbb75cb266b6641e3a9a943
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-11 23:11:35 -0300

    entity professor

--- Entities/Professor.cs
+++ Entities/Professor.cs
@@ -7,5 +7,13 @@ namespace Api.Entities
 {
     public record Professor
     {
+        public Guid Id { get; init; }
+        public string Email { get; init; }
+        public string Name { get; init; }
+        public string Username { get; init; }
+        public string Password { get; init; }
+        public string ProfilePicture { get; init; }
+        public bool EmailVerified { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
     }
 }
