e0159d6e81ed32864c46c530e12753c79640a3ee
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-26 00:07:51 -0300

    tag repository

---
+++ Api/Repositories/TagRepository.cs
@@ -0,0 +1,24 @@
+﻿using Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Repositories
+{
+    public class TagRepository
+    {
+        private readonly List<Tag> tags = new();
+
+        public async Task<IEnumerable<Tag>> GetTag()
+        {
+            return await Task.FromResult(tags);
+        }
+
+        public async Task CreateTag(Tag tag)
+        {
+            tags.Add(tag);
+            await Task.CompletedTask;
+        }
+    }
+}
