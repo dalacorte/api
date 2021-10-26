78c29a892c429577840fcb72c88eaf6b08ac2a11
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-26 00:08:16 -0300

    tag controller

---
+++ Api/Controllers/TagsController.cs
@@ -0,0 +1,48 @@
+﻿using Api.Dto;
+using Api.DTO.Tag;
+using Api.Entities;
+using Api.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("tags")]
+    public class TagsController : ControllerBase
+    {
+        private readonly ITagRepository repository;
+
+        public TagsController(ITagRepository tagRepository)
+        {
+            repository = tagRepository;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IEnumerable<TagDTO>> GetTag()
+        {
+            var tags = (await repository.GetTag()).Select(tag => tag.AsTagDTO());
+
+            return tags;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult<TagDTO>> CreateTag(TagDTO tagDTO)
+        {
+            Tag tag = new()
+            {
+                Tags = tagDTO.Tags
+            };
+
+            await repository.CreateTag(tag);
+
+            return CreatedAtAction(nameof(CreateTag), new { tag = tag.Tags }, tag.AsTagDTO());
+        }
+    }
+}
