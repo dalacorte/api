5eae2d63eaad4cef8487299924d4ea637abb7fd3
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-26 00:08:24 -0300

    tag repository

---
+++ Api/Repositories/Interfaces/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Repositories
+{
+    public interface ITagRepository
+    {
+        Task<IEnumerable<Tag>> GetTag();
+        Task CreateTag(Tag tag);
+    }
+}
