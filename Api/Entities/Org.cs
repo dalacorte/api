1e0de6c6ed35071151abe8aef98392646602f212
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-26 19:23:39 -0300

    org entity

---
+++ Api/Entities/Org.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Entities
+{
+    public record Org
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public User Administrator { get; init; }
+        public User Moderator { get; init; }  
+        public IEnumerable<User> Users { get; init; }    
+        public IEnumerable<Post> Posts { get; init; }
+        public bool Private { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
