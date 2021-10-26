66395fe04810a3329a4be662349cd4ff6846b752
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-26 00:08:09 -0300

    tag dto

---
+++ Api/DTO/Tag/TagDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.DTO.Tag
+{
+    public record TagDTO
+    {
+        [Required]
+        public IEnumerable<String> Tags { get; init; }
+    }
+}
