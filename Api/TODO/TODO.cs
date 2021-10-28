c2a83b20463d120ce17984c7361b1b7aeef29557
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-27 22:39:14 -0300

    todo

--- Api/TODO/TODO.cs
+++ Api/TODO/TODO.cs
@@ -3,9 +3,11 @@
     public class TODO
     {
         // TODO: Org -> Update Users - Moderators
+        //           -> Delete Users - Moderators
         //           -> Change Moderator to Moderators and IEnumerable
 
         // TODO: Tag -> Update - Delete 
+        //
         //           -> Define tags
 
         // TODO: Discussion -> Create DiscussionController, DTO, Interface, Repository...  
